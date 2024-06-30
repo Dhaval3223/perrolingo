81707906414f37478793d9aa22466a8ee6de1a3c
dhavalShiv3223 <dhaval.mulasheeya@shivlab.com>
2024-07-01 01:15:08 +0530

    feat: added flash card table

--- src/layouts/dashboard/header/AccountPopover.tsx
+++ src/layouts/dashboard/header/AccountPopover.tsx
@@ -100,7 +100,7 @@ export default function AccountPopover() {
 
         <Divider sx={{ borderStyle: 'dashed' }} />
 
-        <Stack sx={{ p: 1 }}>
+        {/* <Stack sx={{ p: 1 }}>
           {OPTIONS.map((option) => (
             <MenuItem key={option.label} onClick={() => handleClickItem(option.linkTo)}>
               {option.label}
@@ -108,7 +108,7 @@ export default function AccountPopover() {
           ))}
         </Stack>
 
-        <Divider sx={{ borderStyle: 'dashed' }} />
+        <Divider sx={{ borderStyle: 'dashed' }} /> */}
 
         <MenuItem onClick={handleLogout} sx={{ m: 1 }}>
           Logout
